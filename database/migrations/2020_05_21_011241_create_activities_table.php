230b027fca0148b68dd48652f964e19b98b53902
omarlk <omar.lekouch@gmail.com>
2020-05-21 22:05:38 +0000

    first-commit

---
+++ database/migrations/2020_05_21_011241_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('designation');
+            $table->dateTime('date');
+            $table->string('location');
+            $table->unsignedBigInteger('club_id');
+            $table->foreign('club_id')->references('id')->on('clubs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
---
+++ database/migrations/2020_05_21_011356_create_participations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('date');
+            $table->unsignedBigInteger('activity_id');
+            $table->foreign('activity_id')->references('id')->on('activities');
+
+            $table->unsignedBigInteger('member_id');
+            $table->foreign('member_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participations');
+    }
+}
